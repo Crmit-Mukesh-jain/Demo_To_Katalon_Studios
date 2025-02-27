ac152035dbe07c1e92ef0fb4105d37f41eeec42b
Crmit-Mukesh-jain <mukesh.jain@crmit.com>
2025-02-27 17:26:21 +0530

    Few changes on the project

--- Test Suites/Manager Test Suite.ts
+++ Test Suites/Manager Test Suite.ts
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>fa8b38cf-9552-4f43-a6e7-adb926ecb5fb</testSuiteGuid>
+   <testCaseLink>
+      <guid>588d97d4-2cb9-41b3-a7f7-7aebd559f43b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Credential/Manager_Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
